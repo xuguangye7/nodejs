97b2e87a04778062f9ff1ac3a6a8e1670940960b
wangding <408542507@qq.com>
2019-09-04 18:38:55 +0800

    1

---
+++ 01-introduction/02-hello-world-server.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+console.log('hello world!');
+
+const http = require('http');
+
+http.createServer((req,res)=>{
+  res.end('hello world!');
+}).listen(8080);
---
+++ 02-gloabl-var/01-file-dir-name.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+console.log('hello world!')
+
+console.log('dirname:',__dirname);
+console.log('filename:',__filename);
+
+
+var file = __dirname + '/data/db.xml';
+console.log('file name',file);
+
+var file = __dirname + '\\data\\db.xml';
+console.log('file name in window',file);
