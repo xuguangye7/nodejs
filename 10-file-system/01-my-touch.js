0c4b3f6dae22413fe6c5d99b6306ce2481923569
xuguangye7 <732654087@qq.com>
2019-09-18 16:49:34 +0800

    1

--- 10-file-system/01-my-touch.js
+++ 10-file-system/01-my-touch.js
@@ -1,2 +1,7 @@
 #!/usr/bin/node
 console.log('hello world!')
+
+const fs = require('fs'),
+      file = process.argv[2];
+
+fs.writeFileSync(file, '');
---
+++ 10-file-system/02-my-mkdir.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+console.log('hello world!')
+
+const fs = require('fs'),
+      dir = process.argv[2];
+
+if(typeof(dir) === 'undefined') {
+    console.error('没有指定要创建的目录名称！');
+      process.exit(1);
+}
+
+fs.mkdirSync(dir);
