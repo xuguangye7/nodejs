e45bf715bf8226c5f653e82e83b400e3e87595e1
xuguangye7 <732654087@qq.com>
2019-10-15 14:35:19 +0800

    1

--- 作业/zuoye1.js
+++ 作业/zuoye1.js
@@ -6,22 +6,17 @@ const fs = require('fs'),
       path = require('path');
 try{
   if(dir == 'list' && typeof(dir1)==='undefined'){    
-    //读取当前目录下所有文件的列表
     fs.readdir(__dirname,(err,files)=>{
-      if(err){
-        console.error(err);
-      }else{
-        files.forEach(function(file){
-          fs.stat(file,(err,stats)=>{//获取文件信息
-            var size = stats.size;
-            console.log('{"fileName":"%s","fileSize":"%s"}',file,size);//按要求输出
-          })
+      files.forEach(function(file){
+        fs.stat(file,(err,st)=>{
+          var size = st.size;
+          console.log('{"fileName":"%s","fileSize":"%s"}',file,size);
         })
-      }
+      })
     })
   }
   else if(dir == 'mkdir' && dir2 == 'folder'){
-    fs.mkdirSync('folder');//创建目录
+    fs.mkdirSync('folder');
   }
   else if(typeof(dir) === 'undefined'){
     console.error('没有输入命令行参数！');
